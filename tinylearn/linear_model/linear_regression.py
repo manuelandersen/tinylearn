59f7445581a4f8cc3ae0d8c2f9945c4eec40bb1f
manuelandersen <manuelandersen9@gmail.com>
2024-04-01 17:16:40 -0300

    Update linear_regresson.py

--- tinylearn/linear_model/linear_regression.py
+++ tinylearn/linear_model/linear_regression.py
@@ -8,11 +8,24 @@ class LinearRegression():
         
         self.lr = lr
         self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
 
     def fit(self, X, y):
         num_samples, num_features = X.shape
         self.weigths = np.random(num_features)
-        self.biases = 0
+        self.bias = 0
+
+        for i in range(self.n_iters):
+            y_pred = np.dot(X, self.weights) + self.bias
+
+            dw = (1 / num_samples) * np.dot(X.T, y_pred - y)
+            db = (1 / num_samples) * np.sum(y_pred - y)
+
+            self.weights = self.weights - self.lr * dw
+            self.bias = self.bias - self.lr * db
+
+        return self
 
     def predict(self, X):
-        return np.dot(X, self.weights) + self.biases
+        return np.dot(X, self.weights) + self.bias
